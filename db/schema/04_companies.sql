c0308a28452051d184f299b6996749f6fdf5e2e4
Adib Zefar <adibzefar@gmail.com>
2022-08-18 18:43:43 -0400

    Add database tables

---
+++ db/schema/04_companies.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS companies CASCADE;
+
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
+);
