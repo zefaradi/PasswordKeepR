9e840a8b2f9be3b71cf9decb163412004c15a32c
trevorfunk <trevorjonfunk@gmail.com>
2022-08-22 14:20:40 -0700

    add jQuery to edit_site

--- db/schema/05_favourites.sql
+++ db/schema/05_favourites.sql
@@ -5,3 +5,5 @@ CREATE TABLE favourites (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   company_id INTEGER REFERENCES companies(id) ON DELETE CASCADE
 );
+
+
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,14 @@
 // Client facing scripts here
 $(document).ready(function() {
 
-//-------------------------- CREATE NEW WEBSITE PAGE ----------------------------------------
-//   $('.user-info').hide();
+//---------------- HIDE USERNAME AND PASSWORD ON EDIT PAGE -----------------------------
+  $('.hide-form').hide();
+  $('.edit-submit').hide();
 })
+
+// SHOW USER NAME AND PASSWORD ON EDIT PAGE ---------------------------------------------
+ $(".show").on("click", function(event){
+  $(".hide-form").show();
+  $(".show").hide();
+  $('.edit-submit').show();
+});
