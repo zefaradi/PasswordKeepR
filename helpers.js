c8b33070042e9c0e0aeed1af7146a09c136c399f
Adib Zefar <adibzefar@gmail.com>
2022-08-25 16:55:08 -0400

    Add check for company in helpers and updated getUserByEmail to reflect the changes in the table schema

--- helpers.js
+++ helpers.js
@@ -7,23 +7,37 @@ const pool = new Pool({
 });
 
 //Checks if an E-mail already exists in the user database
-const getUserByEmail  = (email) => {
-    return pool
+const getUserByEmail = (email) => {
+  return pool
     .query(`SELECT * FROM users WHERE email = $1`, [email])
-    .then ((result) => {
-        // console.log("line 16:",result.rows[0])
-        return result.rows;
+    .then((result) => {
+      // console.log("line 16:",result.rows[0])
+      return result.rows;
     })
 
     .catch((err) => console.log(err.message));
+};
 
-    // for (let user in database) {
-    //   if (database[user].email === email) {
-    //     return database[user];
-    //   }
-    // }
-    // return undefined;
-  };
+const checkForCompany = async (companyName, categoryList) => {
+  let companyId;
+  try {
+    const result = await pool.query(`SELECT * FROM companies where companies.name = $1`, [companyName])
+    if (result.rows.length === 0) {
+      const company = await pool
+        .query(`INSERT INTO companies (name, category_id)
+        VALUES ($1, $2) RETURNING*`, [companyName, categoryList])
+      companyId = company.id
+    } else {
+      companyId = result.rows[0].id
+    }
+    return companyId
+  } catch {
+    (error) => {
+      console.log(error.message)
+    }
+  }
+}
 
-  exports.getUserByEmail = getUserByEmail;
+
+module.exports = { getUserByEmail, checkForCompany };
 
