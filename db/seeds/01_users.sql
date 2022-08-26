d592338703c68c5feeb2ac879f971921ded35992
Adib Zefar <adibzefar@gmail.com>
2022-08-26 19:21:22 -0400

    made random password generator to show password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (email, password) VALUES ('Alice', '1');
-INSERT INTO users (email, password) VALUES ('Kira', '456def');
+INSERT INTO users (email, password) VALUES ('Alice@email.com', '1');
+INSERT INTO users (email, password) VALUES ('Kira@email.com', '2');
--- db/seeds/03_companies.sql
+++ db/seeds/03_companies.sql
@@ -1,2 +1,2 @@
 INSERT INTO companies (name, category_id)
-VALUES ('google', 2), ('youtube', 1), ('slack', 3);
+VALUES ('google', 2), ('youtube', 1), ('slack', 3), ('linkedin', 2), ('twitter', 3);
